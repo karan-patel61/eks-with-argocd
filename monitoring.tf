3c0f4e64df03c36546d7ce4e0cb08fe4ed96ee08
karan-patel61 <patel_karan_cool@hotmail.com>
2026-04-29 22:56:54 -0400

    added prometheus & grafana to EKS cluster

---
+++ monitoring.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_namespace_v1" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  version    = "61.3.1" # Standard stable version
+
+  # This allows Grafana to be accessed via an AWS Load Balancer
+  set = [{
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  },
+  {
+    # Ensure Prometheus has enough storage for history (using AWS EBS)
+    name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage"
+    value = "10Gi"
+  }]
+
+  # Ensure Prometheus has enough storage for history (using AWS EBS)
+#   set = {
+#     name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage"
+#     value = "10Gi"
+#   }
+
+  depends_on = [module.eks]
+}
