9b9566185f113ab096f7d8e9d4fdbda0a28a43b0
karan-patel61 <patel_karan_cool@hotmail.com>
2026-05-13 21:02:17 -0400

    install aws load balancer using helm

--- lb-controller.tf
+++ lb-controller.tf
@@ -4,6 +4,9 @@ resource "helm_release" "lbc" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
   version    = "1.7.2" # Check for latest version in 2026
+  # Increase timeout to 15 minutes
+#   timeout    = 900
+#   wait       = true
 
   set = [{
     name  = "clusterName"
@@ -11,14 +14,14 @@ resource "helm_release" "lbc" {
   },
   {
     name  = "serviceAccount.create"
-    value = "false"
+    value = "true"
   },
   {
     name  = "serviceAccount.name"
     value = "aws-load-balancer-controller"
   },
   {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.io/role-arn"
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = module.lbc_irsa_role.iam_role_arn
   },
   # This ensures the CRDs are installed
